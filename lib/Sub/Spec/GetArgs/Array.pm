8a557b54b3fb1b3646c8297c278a6a53c0f18ce5
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-01-20 15:54:25 +0700

    v0.06

--- lib/Sub/Spec/GetArgs/Array.pm
+++ lib/Sub/Spec/GetArgs/Array.pm
@@ -123,6 +123,12 @@ __END__
 
 =head1 DESCRIPTION
 
+B<NOTICE>: This module and the L<Sub::Spec> standard is deprecated as of Jan
+2012. L<Rinci> is the new specification to replace Sub::Spec, it is about 95%
+compatible with Sub::Spec, but corrects a few issues and is more generic.
+C<Perinci::*> is the Perl implementation for Rinci and many of its modules can
+handle existing Sub::Spec sub specs.
+
 This module provides get_args_from_array() (and gencode_get_args_from_array(),
 upcoming). This module is used by, among others, L<Sub::Spec::GetArgs::Argv> and
 L<Sub::Spec::Wrapper>.
